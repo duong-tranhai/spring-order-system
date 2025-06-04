2b38949167ca1ae49f5699aba59db6677c66a9c8
Namanmyxao <vietcongsd999@gmail.com>
2025-06-04 16:49:56 +0700

    Add files via upload

---
+++ nashtech/training/ordersystem/security/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package nashtech.training.ordersystem.security;
+
+import nashtech.training.ordersystem.entity.User;
+import nashtech.training.ordersystem.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles().stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList())
+        );
+    }
+}
+
---
+++ nashtech/training/ordersystem/service/OrderService.java
@@ -0,0 +1,104 @@
+package nashtech.training.ordersystem.service;
+
+import nashtech.training.ordersystem.dto.*;
+import nashtech.training.ordersystem.entity.*;
+import nashtech.training.ordersystem.repository.*;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+//public interface OrderService {
+//}
+
+@Service
+public class OrderService{
+    private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
+    private final UserRepository userRepository;
+
+    public OrderService(OrderRepository orderRepository,
+                        ProductRepository productRepository,
+                        UserRepository userRepository){
+        this.orderRepository = orderRepository;
+        this.productRepository = productRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public OrderResponseDTO createOrder(String customerUsername, OrderRequestDTO orderRequestDTO){
+        User customer = userRepository.findByUsername(customerUsername).orElseThrow(()->new RuntimeException("Customer not found"));
+        Order order = new Order();
+        order.setCustomer(customer);
+        order.setStatus(OrderStatus.PENDING);
+
+        List<OrderItem> orderItems = orderRequestDTO.items().stream().map(itemDTO -> {
+            Product product = productRepository.findById(itemDTO.productId())
+                    .orElseThrow(()-> new RuntimeException("Product not found" + itemDTO.productId()));
+            if (product.getStock() < itemDTO.quantity()){
+                throw new RuntimeException("Insufficient stock for product: " + product.getName());
+            }
+
+            product.setStock(product.getStock() - itemDTO.quantity());
+            productRepository.save(product);
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+            orderItem.setProduct(product);
+            orderItem.setQuantity(itemDTO.quantity());
+            orderItem.setPrice(product.getPrice());
+            return orderItem;
+        }).collect(Collectors.toList());
+
+        order.setItems(orderItems);
+
+        Order savedOrder = orderRepository.save(order);
+        return toResponseDTO(savedOrder);
+    }
+
+    public List<OrderResponseDTO> getOrdersForUser(String username, Role role){
+        List<Order> orders;
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(()-> new RuntimeException("User not found"));
+        
+        if (role == Role.ROLE_CUSTOMER){
+            orders = orderRepository.findByCustomer(user);
+        } else if (role == Role.ROLE_SELLER) {
+            orders = orderRepository.findBySeller(user);
+        } else{
+            orders = orderRepository.findAll();
+        }
+
+        return orders.stream().map(this::toResponseDTO).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public OrderResponseDTO updateOrderStatus(Long orderId, OrderStatus newStatus){
+        Order order = orderRepository.findById(orderId).orElseThrow(()-> new RuntimeException("Order not found"));
+        order.setStatus(newStatus);
+        Order updated = orderRepository.save(order);
+        return toResponseDTO(updated);
+    }
+
+    private OrderResponseDTO toResponseDTO(Order order){
+        List<OrderItemResponseDTO> items = order.getItems().stream()
+                .map(oi -> new OrderItemResponseDTO(
+                        oi.getProduct().getId(),
+                        oi.getProduct().getName(),
+                        oi.getQuantity(),
+                        oi.getPrice()
+                )).collect(Collectors.toList());
+        String sellerUsername = order.getSeller() != null ? order.getSeller().getUsername(): null;
+
+        return new OrderResponseDTO(
+                order.getId(),
+                order.getCustomer().getUsername(),
+                sellerUsername,
+                order.getStatus().name(),
+                items,
+                order.getCreatedAt()
+        );
+    }
+
+}
